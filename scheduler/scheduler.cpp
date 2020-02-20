917d11e43bc4114aab9c6cc65d1aaa114c1f3666
kevin <kevin@ubuntu>
2020-02-20 13:17:26 -0800

    Implemented Scheduler

--- scheduler/scheduler.cpp
+++ scheduler/scheduler.cpp
@@ -9,17 +9,24 @@
 
 
 void Scheduler::add(PCB p) {
-
+	ready_q->push(p);
 }
 
 PCB Scheduler::getNext() {
-	return PCB();
+	if (ready_q-> empty())
+		return PCB();
+
+	PCB p = ready_q->front();
+	ready_q->pop();
+
+	return p;
 }
 
 bool Scheduler::isEmpty() {
-	return false;
+	return ready_q->empty();
 }
 
+//tick_count necessary?
 bool Scheduler::time_to_switch_processes(int tick_count, PCB &p) {
-	return false;
+	return (p.remaining_cpu_time <= 0);
 }
