686b2ae19f2711c8b682ec2cb2bc193767a9161d
kevin <kevin@ubuntu>
2020-02-20 11:26:07 -0800

    Implemented dispatcher.cpp

--- dispatcher/dispatcher.cpp
+++ dispatcher/dispatcher.cpp
@@ -8,15 +8,19 @@
 #include "../includes/dispatcher.h"
 
 
-
 PCB Dispatcher::get_from_CPU() {
-	return PCB();
+	if (!isValidJobOnCPU())
+		return PCB();
+
+	return cpu->get_process_off_core();
 }
 
 void Dispatcher::put_on_CPU(PCB &process) {
-
+	if(!isValidJobOnCPU())
+		cpu->put_process_on_core(process);
 }
 
 bool Dispatcher::isValidJobOnCPU() {
-	return false;
+	is_valid_job_on_cpu = !cpu->get_COPY_of_Current_Process().isEmpty();
+	return is_valid_job_on_cpu;
 }
