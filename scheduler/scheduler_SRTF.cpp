dfa258ea4758926c55149453b2ca670ef1b4ac6b
kevin <kevin@ubuntu>
2020-02-20 13:44:29 -0800

    I am having trouble with srtf. The sorting function is messing me up a ton. Got to put more time into it.

--- scheduler/scheduler_SRTF.cpp
+++ scheduler/scheduler_SRTF.cpp
@@ -10,10 +10,20 @@
 
 #include "../includes/scheduler_SRTF.h"
 
-bool Scheduler_SRTF::time_to_switch_processes(int tick_count, PCB &b) {
-	return false;
+
+bool Scheduler_SRTF::time_to_switch_processes(int tick_count, PCB &p) {
+	sort();
+
+	int time_remaining = ready_q->front().remaining_cpu_time;
+
+	if (time_remaining < 0 || time_remaining < p.remaining_cpu_time)
+		return true;
+
+	return Scheduler::time_to_switch_processes;
 }
 
 void Scheduler_SRTF::sort() {
 
 }
+
+
