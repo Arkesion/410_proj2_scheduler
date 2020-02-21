4e44b5c1e91bb335787b8408a5088ab2a886fd6a
kevin <kevin@ubuntu>
2020-02-20 21:26:07 -0800

    After getting a hint about priority queues, I have finished the projecto. Thank Goodness

--- scheduler/scheduler_SRTF.cpp
+++ scheduler/scheduler_SRTF.cpp
@@ -10,7 +10,6 @@
 
 #include "../includes/scheduler_SRTF.h"
 
-
 bool Scheduler_SRTF::time_to_switch_processes(int tick_count, PCB &p) {
 	sort();
 
@@ -19,11 +18,26 @@ bool Scheduler_SRTF::time_to_switch_processes(int tick_count, PCB &p) {
 	if (time_remaining < 0 || time_remaining < p.remaining_cpu_time)
 		return true;
 
-	return Scheduler::time_to_switch_processes;
+	return Scheduler::time_to_switch_processes(tick_count, p);
 }
+
+struct compare_remaining_time {
+	bool operator()(PCB x, PCB y) {
+		return x.remaining_cpu_time > y.remaining_cpu_time;
+	}
+};
 
 void Scheduler_SRTF::sort() {
+	std::priority_queue<PCB, std::vector<PCB>, compare_remaining_time> sorted;
 
+	while (!ready_q->empty()) {
+		sorted.push(ready_q->front());
+		ready_q->pop();
+	}
+
+	while (!sorted.empty()) {
+		ready_q->push(sorted.top());
+		sorted.pop();
+	}
 }
-
 
